e95a2d6097cb3f9ba039e21b3c73a7257b4bc7b4
KimSunWooo <88176103+KimSunWooo@users.noreply.github.com>
2026-09-08 18:28:00 +0900

    MODIFY next.config.ts (NEXT IMAGE ERROR)

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,25 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   images: {
     remotePatterns: [
+      // 1. hince 도메인
       {
         protocol: 'https',
-        hostname: 'hince.co.kr', // 허용할 크롤링 대상 도메인
+        hostname: 'hince.co.kr', 
       },
-    ],
-  },
-
-};
-// next.config.js
-module.exports = {
-  images: {
-    remotePatterns: [
+      // 2. S3 도메인 (추가됨)
       {
         protocol: 'https',
-        hostname: '2026portfolio.s3.ap-northeast-2.amazonaws.com', // 자신의 S3 도메인으로 변경
+        hostname: '2026portfolio.s3.ap-northeast-2.amazonaws.com', 
         pathname: '/**',
       },
     ],
